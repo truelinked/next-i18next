0fede90e6ba797ffae8f33a707452b3e64783663
i.maslakoff <igormaslakoff@gmail.com>
2020-03-02 15:46:36 +0300

    fix add-subpath: support search query

--- dist/es/utils/add-subpath.js
+++ dist/es/utils/add-subpath.js
@@ -1,1 +1,8 @@
-export const addSubpath = (url, subpath) => url.concat(`/${subpath}/`).replace(/(https?:\/\/)|(\/)+/g, "$1$2").replace(/\/$/, '');
+import { format, parse } from 'url';
+export const addSubpath = (url, subpath) => {
+  const parsedURL = parse(url);
+  const formattedURL = format({ ...parsedURL,
+    pathname: parsedURL.pathname.concat(`/${subpath}`)
+  });
+  return formattedURL.replace(/(https?:\/\/)|(\/)+/g, "$1$2").replace(/\/$/, '');
+};
--- src/utils/add-subpath.ts
+++ src/utils/add-subpath.ts
@@ -1,4 +1,9 @@
-export const addSubpath = (url: string, subpath: string) =>
-    url.concat(`/${subpath}/`)
-        .replace(/(https?:\/\/)|(\/)+/g, "$1$2")
-        .replace(/\/$/, '')
+import { format, parse } from 'url'
+
+export const addSubpath = (url: string, subpath: string) => {
+  const parsedURL = parse(url)
+  const formattedURL = format({ ...parsedURL, pathname: parsedURL.pathname.concat(`/${subpath}`)})
+  return formattedURL
+    .replace(/(https?:\/\/)|(\/)+/g, "$1$2")
+    .replace(/\/$/, '')
+}
